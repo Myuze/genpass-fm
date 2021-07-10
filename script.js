39be9a9be294add895596e0128746225a3f89b71
Florian Meneses <florian.meneses@gmail.com>
2021-07-09 21:07:49 -0700

    message and criteria methods added

--- script.js
+++ script.js
@@ -1,97 +1,126 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Prompt Messages
-var messages = {
-    chooseLenght: "Select a password lenght. Must be 8 to 128 characters.",
-    mustBeLenght: "You must select a lenght between 8 and 128 characters.",
-    criteriaMessage: {
-        includeLower: "Include Lowercase?",
-        includeUpper: "Include Uppercase?",
-        includeNumeric: "Include Numeric?",
-        includeSpecialChar: "Include Special Characters?"
-    },   
-    mustSelectOne: "You must select at least one character type."
-}
-
 // Default passCriteria
 var passCriteria = {
     passLength: 0,
     includeLower: true,
     includeUpper: true,
     includeNumeric: true,
-    includeSpecialChar: true
+    includeSpecialChar: true,
+
+    isValid: function () {
+        if (this.includeLower ||
+            this.includeUpper ||
+            this.includeNumeric ||
+            this.includeSpecialChar) {
+                return true;
+            } else {
+                alert(messages.mustSelectOne);
+                return false;
+        }
+    }
+}
+
+// Prompt Messages
+var messages = {
+    chooseLenght: "Select a password lenght. Must be 8 to 128 characters.",
+    mustBeLenght: "You must select a lenght between 8 and 128 characters.",
+    criteriaMessage: {
+        includeLower: "Include Lowercase? y/n",
+        includeUpper: "Include Uppercase? y/n",
+        includeNumeric: "Include Numeric? y/n",
+        includeSpecialChar: "Include Special Characters? y/n"
+    },   
+    invalid: "Invalid selection, must be 'y' or 'n'",
+    mustSelectOne: "You must select at least one character type.",
+
+    promptPassLenght: function () {
+        do {
+            choiceLenght = prompt(this.chooseLenght);
+            if (choiceLenght == null) {
+                return;
+            }
+    
+            if (choiceLenght < 8 || choiceLenght > 128) {
+                alert(this.mustBeLenght)
+            }
+        } while (choiceLenght < 8 || choiceLenght > 128);
+    
+        return choiceLenght;
+    },
+
+    promptPassCriteria: function() {
+        for (var key in this.criteriaMessage) {
+            do {
+                var response = prompt(this.criteriaMessage[key]);
+                var responseIsValid = this.isValidResponse(response)
+                
+                if (response == null) {
+                    passCriteria[key] = false;
+                
+                } else if (
+                    response.toLowerCase() == 'y' ||
+                    response.toLowerCase() == 'yes'
+                    ) {
+                        passCriteria[key] = true;
+                
+                } else if (
+                    response.toLowerCase() == 'n' ||
+                    response.toLowerCase() == 'no'
+                    ) {
+                        passCriteria[key] = false;
+    
+                } else {
+                    alert(messages.invalid);
+                }
+    
+            } while (!responseIsValid);
+        }
+    },
+
+    isValidResponse: function (response) {
+        if (response == null) {
+            return false;
+        }
+    
+        if (response.toLowerCase() == 'y' ||
+            response.toLowerCase() == 'yes' ||
+            response.toLowerCase() == 'n' ||
+            response.toLowerCase() == 'no'
+            ) {
+                return true;
+            } else {   
+                return false;
+        }
+    }
 }
 
 // Write password to the #password input
 function writePassword() {
-    console.log("Write Password RUN")
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
-}
-
-function promptUser(message) {
-    var response = window.prompt(message);
-    return response;
 }
 
 function randomizeCharacter() {
-    console.log("Randomized RUN")
+    console.log("Randomized RUN");
 }
 
 function getPassCriteria() {
-    passCriteria.passLength = promptPassLenght();
-    passCriteria = promptPassCriteria();
-    console.log(passCriteria)
-}
-
-function promptPassCriteria() {
-    for (var key in messages.criteriaMessage) {
-        do {
-            var response = promptUser(messages.criteriaMessage[key]);
-
-        } while (!validatePassCriteria())
-
+    passCriteria.passLength = messages.promptPassLenght();
+    messages.promptPassCriteria();
+    console.log(passCriteria.includeLower);
+    console.log(passCriteria.includeUpper);
+    console.log(passCriteria.includeNumeric);
+    console.log(passCriteria.includeSpecialChar);
+    if (passCriteria.isValid()) {
+        alert("WE FIRE")
     }
 }
 
-function validatePassCriteria () {
-    if (getPassCriteria.includeLower ||
-        getPassCriteria.includeUpper ||
-        getPassCriteria.includeNumeric ||
-        getPassCriteria.includeSpecialChar) {
-            return true;
-        } else {
-            alert(messages.mustSelectOne);
-            return false;
-        }
-}
-
-function promptPassLenght () {
-    console.log("Get Pass Lenght RUN")
-    var choiceLenght = passCriteria.passLength;
-
-    do {
-        choiceLenght = promptUser(messages.chooseLenght)
-        if (choiceLenght == null) {
-            return;
-        }
-
-        if (choiceLenght < 8 || choiceLenght > 128) {
-            alert(messages.mustBeLenght)
-        } else {
-            alert(`${choiceLenght} chosen!`)
-        }
-    } while (choiceLenght < 8 || choiceLenght > 128);
-
-    return choiceLenght;
-}
-
 function generatePassword() {
-    console.log("GeneratePassword RUN")
     var genPass = "derp";
     genPass = getPassCriteria();
     return genPass;
