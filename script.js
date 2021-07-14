540051f9bc7701a71ec5c5087732846e7ca5b097
Florian Meneses <florian.meneses@gmail.com>
2021-07-14 02:05:34 -0700

    added random character functions

--- script.js
+++ script.js
@@ -3,36 +3,67 @@ var generateBtn = document.querySelector("#generate");
 
 // Default passGenerator
 var passGenerator = {
-    passLength: 128,
+    passLength: 129,
     criteria: {
         includeLower: true,
         includeUpper: true,
         includeNumeric: true,
-        includeSpecialChar: true
+        includeSymbol: true,
     },
 
-    get: function () {
+    randomizerName: {
+        lower: this.getLower,
+        upper: this.getUpper,
+        num: this.getNum,
+        sym: this.getSym
+    },
+    charPool: "",
+
+    getCriteria: function () {
         this.passLength = messages.promptPassLength();
         messages.promptPassCriteria();
-        
-        // Debug logging
-        // console.log(this.criteria.includeLower);
-        // console.log(this.criteria.includeUpper);
-        // console.log(this.criteria.includeNumeric);
-        // console.log(this.criteria.includeSpecialChar);
     },
+
+    getLower: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 97);
+    },
+
+    getUpper: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 65);
+    },
+
+    getNum: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    },
+
+    getSym: function () {
+        var symbols =  " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~\"";
+        console.log(symbols)
+        return symbols[Math.floor(Math.random() * symbols.length)];
+    },
+
     generator: function () {
 
-        for (var [key, value] of Object.entries(this.criteria)) {
+        var password = '';
+        var typesCount = this.criteria.includeLower + this.criteria.includeUpper + this.criteria.includeNumeric + this.criteria.includeSymbol;
 
-            console.log(`${key}: ${value}`);
-        }
+        console.log((typesCount));
+        // for (var [key, value] of Object.entries(this.criteria)) {
+        //     console.log(`${key}: ${value}`);
+        // }
+        // for (i = 0; i < this.passLength; i++) {
+
+        // }
+
+        // var typesCount = inner + upper + num + sym;
+
+        return typesCount;
     },
 
     generatePassword: function () {
-        this.get();
+        this.getCriteria();
         if (this.isValid()) {
-            genPass = this.generator();
+            var genPass = this.generator();
         }
         return genPass;
     },
@@ -41,7 +72,7 @@ var passGenerator = {
         if (this.criteria.includeLower ||
             this.criteria.includeUpper ||
             this.criteria.includeNumeric ||
-            this.criteria.includeSpecialChar) {
+            this.criteria.includeSymbol) {
                 return true;
             } else {
                 alert(messages.mustSelectOne);
@@ -54,38 +85,47 @@ var passGenerator = {
 var messages = {
     chooseLength: "Select a password length. Must be 8 to 128 characters.",
     mustBeLength: "You must select a length between 8 and 128 characters.",
+    mustBeNumber: "You must select a number between 8 and 128 characters.",
     criteriaMessage: {
         includeLower: "Include Lowercase? y/n",
         includeUpper: "Include Uppercase? y/n",
         includeNumeric: "Include Numeric? y/n",
-        includeSpecialChar: "Include Special Characters? y/n"
+        includeSymbol: "Include Special Characters? y/n"
     },   
     invalid: "Invalid selection, must be 'y' or 'n'",
     mustSelectOne: "You must select at least one character type.",
+    canceled: "Canceled Password Generation.",
 
     promptPassLength: function () {
+        
         do {
-            choiceLength = prompt(this.chooseLength);
-            if (choiceLength == null) {
-                return;
+            this.passLength = Number(prompt(this.chooseLength));
+ 
+            // Reprompt if not at least 8 to 128
+            if (this.passLength < 8 || this.passLength > 128) {
+                alert(this.mustBeLength);
+            } else if (isNaN(this.passLength)) {
+                alert(this.mustBeNumber);
+            } else {
+                alert(this.canceled);
+                return null;
             }
+        } while (this.passLength < 8 || this.passLength > 128 || isNaN(this.passLength));
     
-            if (choiceLength < 8 || choiceLength > 128) {
-                alert(this.mustBeLength)
-            }
-        } while (choiceLength < 8 || choiceLength > 128);
-    
-        return choiceLength;
+        return this.passLength;
     },
 
     promptPassCriteria: function() {
         for (var key in this.criteriaMessage) {
+            
             do {
+                
                 var response = prompt(this.criteriaMessage[key]);
                 var responseIsValid = this.isValidResponse(response)
                 
-                if (response == null) {
-                    passGenerator.criteria[key] = false;
+                if (response === null) {
+                    alert(this.canceled);
+                    return;
                 
                 } else if (
                     response.toLowerCase() == 'y' ||
@@ -108,8 +148,8 @@ var messages = {
     },
 
     isValidResponse: function (response) {
-        if (response == null) {
-            return false;
+        if (response === null) {
+            return null;
         }
     
         if (response.toLowerCase() == 'y' ||
